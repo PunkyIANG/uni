a5370e94f07a3b0ab8622feadeaf440cd17043be
PunkyIANG <tsoorckanukris@gmail.com>
2022-09-30 12:29:58 +0300

    winforms ui

--- task list.md
+++ task list.md
@@ -2,18 +2,18 @@
 - 2/7 Laboratoare
 - 2 atestari
 
-## Paradigme (Wolfram)
-- 4 laboratoare
-
-## Programarea vizuala (UI)
-- 3 laboratoare
-
 ## Wireless
 - ~ referat
 - ~ lab cisco packet tracer
 - 2 atestari
 - teste
 
+## Programarea vizuala (UI)
+- 3 laboratoare
+
 ## arnaut
 - laboratoare
 - ?
+
+## Paradigme (Wolfram)
+- 4 laboratoare
--- winforms/lab1v2/MainForm.cs
+++ winforms/lab1v2/MainForm.cs
@@ -5,19 +5,147 @@ public partial class MainForm : Form
     public MainForm()
     {
         InitializeComponent();
-        var labelArr = new Label[10];
 
-        // foreach (ref var l in labelArr.AsSpan())
-        for (int i = 0; i < labelArr.Length; i++)
+        #region col1
+        var gpuList = new ListBox();
+        gpuList.Height = 330;
+        Controls.Add(gpuList);
+        #endregion
+
+        #region col2
+        var manufacturerDropDown = new ComboBox
         {
-            var l = new Label();
-            l.Text = $"label text {i}";
-            l.Location = new Point(0, i*20);
-            Controls.Add(l);
-        }
+            Location = new Point(130, 0),
+        };
+        Controls.Add(manufacturerDropDown);
 
-        // var b = new Button();
-        // b.Text = "button text";
-        // Controls.Add(b);
+        var modelTextBox = new TextBox
+        {
+            Location = new Point(130, 35),
+            Width = 120,
+            PlaceholderText = "Model"
+        };
+        Controls.Add(modelTextBox);
+
+        var outputButtons = new Button[] {
+            new Button {
+                Text = "VGA",
+                Location = new Point(130, 70),
+                Width = 60,
+                Height = 30,
+            },
+            new Button {
+                Text = "DVI",
+                Location = new Point(190, 70),
+                Width = 60,
+                Height = 30,
+            },
+            new Button {
+                Text = "HDMI",
+                Location = new Point(130, 105),
+                Width = 60,
+                Height = 30,
+            },
+            new Button {
+                Text = "DP",
+                Location = new Point(190, 105),
+                Width = 60,
+                Height = 30,
+            },
+        };
+        Controls.AddRange(outputButtons);
+
+        var outputList = new ListBox
+        {
+            Location = new Point(130, 140),
+            Height = 110,
+        };
+        Controls.Add(outputList);
+
+        var resolutionCheckBoxes = new CheckBox[] {
+            new CheckBox {
+                Location = new Point(130, 255),
+                Text = "Full HD",
+            },
+            new CheckBox {
+                Location = new Point(130, 280),
+                Text = "1440p",
+            },
+            new CheckBox {
+                Location = new Point(130, 305),
+                Text = "4K",
+            },
+        };
+        Controls.AddRange(resolutionCheckBoxes);
+
+        #endregion
+
+        #region col3
+        var priceLabel = new Label
+        {
+            Location = new Point(260, 2),
+            Text = "Price:",
+            Width = 45,
+        };
+        Controls.Add(priceLabel);
+
+        var priceNumericUpDown = new NumericUpDown
+        {
+            Location = new Point(310, 0),
+            Width = 71,
+
+        };
+        Controls.Add(priceNumericUpDown);
+
+
+        var baseClockLabel = new Label
+        {
+            Location = new Point(260, 36),
+            Text = "Clock:",
+            Width = 50,
+        };
+        Controls.Add(baseClockLabel);
+
+        var baseClockNumericUpDown = new NumericUpDown
+        {
+            Location = new Point(310, 35),
+            Width = 71,
+        };
+        Controls.Add(baseClockNumericUpDown);
+
+
+        var memoryTypeDropDown = new ComboBox
+        {
+            Location = new Point(260, 70),
+        };
+        Controls.Add(memoryTypeDropDown);
+
+        var memoryManufacturerDropDown = new ComboBox {
+            Location = new Point(260, 105),
+        };
+        Controls.Add(memoryManufacturerDropDown);
+
+
+        var memorySizeLabel = new Label {
+            Location = new Point(260, 141),
+            Text = "Size:",
+            Width = 50,
+        };
+        Controls.Add(memorySizeLabel);
+
+        var memorySizeNumericUpDown = new NumericUpDown
+        {
+            Location = new Point(310, 140),
+            Width = 71,
+        };
+        Controls.Add(memorySizeNumericUpDown);
+
+        var productionCheckbox = new CheckBox {
+            Location = new Point(260, 175),
+            Text = "Is in active\r\nproduction",
+            Height = 50,
+        };
+        Controls.Add(productionCheckbox);
+        #endregion
     }
 }
