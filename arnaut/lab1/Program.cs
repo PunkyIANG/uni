357569b70ff5eb3c110f432d0fd9ea48cd353991
PunkyIANG <tsoorckanukris@gmail.com>
2022-10-17 10:48:14 +0300

    added input by keyboard

--- arnaut/lab1/Program.cs
+++ arnaut/lab1/Program.cs
@@ -6,32 +6,47 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        bool keyboardInit = args.Length >= 3;
+
         int arrLength = 10;
         if (args.Length >= 2)
-            arrLength = Int32.Parse(args[1]);
+            arrLength = int.Parse(args[1]);
 
         var arrayToSort = Enumerable.Range(0, arrLength).ToArray();
-        var rng = new Random(0);
-        rng.Shuffle<int>(arrayToSort);
+
+        if (keyboardInit)
+        {
+            for (int i = 0; i < arrLength; i++)
+            {
+                Console.Write($"Dati elementul {i}: ");
+                arrayToSort[i] =  int.Parse(Console.ReadLine());
+            }
+        }
+        else
+        {
+            var rng = new Random(0);
+            rng.Shuffle<int>(arrayToSort);
+        }
 
 
         string searchAlgo = "bubble";
         if (args.Length >= 1)
             searchAlgo = args[0];
 
-        switch (searchAlgo) {
-            case "insertion" :
+        switch (searchAlgo)
+        {
+            case "insertion":
                 arrayToSort.InsertionSort();
                 break;
 
-            case "quick" :
+            case "quick":
                 arrayToSort.QuickSort();
-                break;        
+                break;
 
-            case "bubble" :
+            case "bubble":
             default:
                 arrayToSort.BubbleSort();
                 break;
-        }      
+        }
     }
 }
