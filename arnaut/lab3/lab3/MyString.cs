4a33a12b9e6a4323e3b15f4d08484587c0117a78
PikaIANG <tsoorckanukris@gmail.com>
2022-11-27 15:21:29 +0200

    lab3 mostly done

---
+++ arnaut/lab3/lab3/MyString.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+
+namespace lab3;
+
+public class MyString : IEnumerable<char>
+{
+    private readonly char[] _characters;
+
+    #region Constructors
+
+    public MyString(char[] characters)
+    {
+        this._characters = (char[])characters.Clone();
+    }
+
+    public MyString() : this(Array.Empty<char>()) {}
+
+    public MyString(string inputString) : this(inputString.ToCharArray()) { }
+    
+    public MyString(MyString myString) : this(myString._characters) { }
+    
+    #endregion
+
+    #region Indexer
+
+    public char this[int i]
+    {
+        get => _characters[i];
+        set => _characters[i] = value;
+    }
+
+    #endregion
+
+    #region Properties
+    
+    public static MyString Empty => new MyString();
+    public int Length => _characters.Length;
+    public bool IsEmpty => Length == 0;
+    
+    #endregion
+    
+    #region Methods
+    
+    public char[] ToCharArray() => (char[])_characters.Clone();
+    
+    public override string ToString() => new(_characters);
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public MyString Substring(int startIndex, int length)
+    {
+        if (startIndex < 0 || startIndex >= Length)
+            throw new ArgumentOutOfRangeException(nameof(startIndex));
+        if (length < 0 || startIndex + length > Length)
+            throw new ArgumentOutOfRangeException(nameof(length));
+        var result = new char[length];
+        Array.Copy(_characters, startIndex, result, 0, length);
+        return new MyString(result);
+    }
+    
+    public MyString Substring(int startIndex) => Substring(startIndex, Length - startIndex);
+    
+    public MyString Concat(MyString myString)
+    {
+        var result = new char[Length + myString.Length];
+        Array.Copy(_characters, result, Length);
+        Array.Copy(myString._characters, 0, result, Length, myString.Length);
+        return new MyString(result);
+    }
+    
+    
+    public MyString Concat(string str) => Concat(new MyString(str));
+    
+    public MyString Concat(char c) => Concat(new MyString(new[] {c}));
+    
+    public MyString Concat(char[] chars) => Concat(new MyString(chars));
+    
+    public MyString Concat(char[] chars, int startIndex, int length)
+    {
+        if (startIndex < 0 || startIndex >= chars.Length)
+            throw new ArgumentOutOfRangeException(nameof(startIndex));
+        if (length < 0 || startIndex + length > chars.Length)
+            throw new ArgumentOutOfRangeException(nameof(length));
+        var result = new char[Length + length];
+        Array.Copy(_characters, result, Length);
+        Array.Copy(chars, startIndex, result, Length, length);
+        return new MyString(result);
+    }
+    
+    //TODO: rewrite all concat overloads to use this method
+    public MyString Concat(MyString myString, int startIndex, int length)
+    {
+        if (startIndex < 0 || startIndex >= myString.Length)
+            throw new ArgumentOutOfRangeException(nameof(startIndex));
+        if (length < 0 || startIndex + length > myString.Length)
+            throw new ArgumentOutOfRangeException(nameof(length));
+        var result = new char[Length + length];
+        Array.Copy(_characters, result, Length);
+        Array.Copy(myString._characters, startIndex, result, Length, length);
+        return new MyString(result);
+    }
+    
+    public MyString Concat(string str, int startIndex, int length)
+    {
+        if (startIndex < 0 || startIndex >= str.Length)
+            throw new ArgumentOutOfRangeException(nameof(startIndex));
+        if (length < 0 || startIndex + length > str.Length)
+            throw new ArgumentOutOfRangeException(nameof(length));
+        var result = new char[Length + length];
+        Array.Copy(_characters, result, Length);
+        Array.Copy(str.ToCharArray(), startIndex, result, Length, length);
+        return new MyString(result);
+    }
+    
+    public IEnumerator<char> GetEnumerator()
+    {
+        return ((IEnumerable<char>)_characters).GetEnumerator();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is MyString myString)
+            return _characters.Equals(myString._characters);
+        return false;
+    }
+
+    public bool Equals(MyString other)
+    {
+        return _characters.Equals(other._characters);
+    }
+
+    public override int GetHashCode()
+    {
+        return _characters.GetHashCode();
+    }
+
+    #endregion
+    
+    #region Operators
+
+    public static MyString operator+(MyString myString1, MyString myString2) => myString1.Concat(myString2);
+
+    public static bool operator==(MyString myString1, MyString myString2) => myString1.Equals(myString2);
+    
+    public static bool operator!=(MyString myString1, MyString myString2) => !myString1.Equals(myString2);
+
+    public static implicit operator MyString(string str) => new(str);
+    
+    public static implicit operator string(MyString myString) => myString.ToString();
+
+    #endregion
+}
---
+++ arnaut/lab3/lab3/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace lab3;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        #region Constructors
+
+        Console.WriteLine("1. Testam constructorii");
+        
+        Console.WriteLine("Constructor gol: " + new MyString());
+
+        Console.WriteLine("Constructor cu parametru string: " + new MyString("string"));
+        
+        Console.WriteLine($"Constructor cu parametru MyString: {new MyString(new MyString("MyString"))}");
+        
+        Console.Write("Constructor cu parametru char[]: ");
+        Console.WriteLine(new MyString(new[] {'c', 'h', 'a', 'r', 's'}));
+
+        Console.WriteLine();
+        
+        #endregion
+
+        #region Indexer
+
+        var stringToIndex = new MyString("Indexer");
+        
+        Console.WriteLine("2. Testam indexerul");
+        Console.WriteLine("Stringul initial este: " + stringToIndex);
+        
+        stringToIndex[0] = 'i';
+        stringToIndex[1] = 'N';
+        stringToIndex[2] = 'd';
+        stringToIndex[3] = 'E';
+        stringToIndex[4] = 'x';
+        stringToIndex[5] = 'E';
+        stringToIndex[6] = 'r';
+        
+        Console.WriteLine("Stringul dupa modificare este: " + stringToIndex);
+        
+        Console.WriteLine();
+
+        #endregion
+        
+        #region Properties
+        
+        Console.WriteLine("3. Testam proprietatile");
+        
+        Console.WriteLine("String gol prin proprietati: " + MyString.Empty);
+        Console.WriteLine("Lungimea stringului indexer: " + stringToIndex.Length);
+        Console.WriteLine("Este stringul indexer gol? " + stringToIndex.IsEmpty);
+        
+        Console.WriteLine();
+        
+        #endregion
+        
+        #region Methods
+        
+        Console.WriteLine("4. Testam metodele");
+        
+        var stringToCharArray = new MyString("ToCharArray");
+        var charArray = stringToCharArray.ToCharArray();
+        
+        Console.WriteLine("Initiem stringul ToCharArray si il convertim in char[]: " + stringToCharArray + " -> " + new string(charArray));
+        
+        charArray[0] = 't';
+        charArray[1] = 'O';
+        charArray[2] = 'c';
+        charArray[3] = 'H';
+        charArray[4] = 'a';
+        charArray[5] = 'R';
+        charArray[6] = 'a';
+        charArray[7] = 'R';
+        charArray[8] = 'r';
+        charArray[9] = 'A';
+        charArray[10] = 'y';
+        
+        Console.WriteLine("Modificam char[] si il comparam cu stringul ToCharArray: " + stringToCharArray + " -> " + new string(charArray));
+        
+        Console.WriteLine("Convertim MyString in string: " + stringToCharArray + " -> " + stringToCharArray.ToString());
+        
+        Console.WriteLine("Enumeram elementele din MyString: ");
+        
+        foreach (var c in stringToCharArray)
+        {
+            Console.Write(c + " ");
+        }
+        Console.WriteLine();
+        
+        Console.WriteLine("Luam o parte din MyString: " + stringToCharArray.Substring(2, 4));
+        
+        Console.WriteLine("Concatenam parti din stringuri: " + stringToCharArray.Concat(charArray, 0, 2));
+        
+        Console.WriteLine();
+        
+        #endregion
+
+        #region Operators
+
+        var first = new MyString("first");
+        var second = new MyString("second");
+        
+        Console.WriteLine("5. Testam operatorii");
+        Console.WriteLine("Adunam stringurile first si second: " + (first + second));
+        
+        Console.WriteLine("Comparam stringurile first si second: " + (first == second));
+        
+        Console.WriteLine("MyString automat se convertesc in string, altfel nu ar fi printate");
+        #endregion
+    }
+}
